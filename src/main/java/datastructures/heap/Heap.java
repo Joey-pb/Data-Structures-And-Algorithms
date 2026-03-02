ebd7de49d397c0f4d8ffd2f90f18ca36bc313d0b
Joseph <jbas127@wgu.edu>
2026-03-01 20:13:48 -0500

    Add Heap data structure

---
+++ src/main/java/datastructures/heap/Heap.java
@@ -0,0 +1,79 @@
+package datastructures.heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+    List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    public void insert(int value) {
+        heap.add(value);
+        int current = heap.size() - 1;
+
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public Integer remove() {
+        if (heap.isEmpty()) return null;
+        if (heap.size() == 1) return heap.removeFirst();
+
+        int maxValue = heap.getFirst();
+        heap.set(0, heap.removeLast());
+        sinkDown(0);
+
+        return maxValue;
+    }
+
+    // Helper methods
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    private void sinkDown(int index) {
+        int maxIndex = index;
+
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+            if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+            if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+
+            if (maxIndex != index) {
+                swap(maxIndex, index);
+                index = maxIndex;
+            } else {
+                return;
+            }
+        }
+    }
+}
