2a505948bf2bc23b3e7291f23b9fd7bfc520dfc4
Joseph <jbas127@wgu.edu>
2026-03-03 20:53:44 -0500

    Add recursive methods for BinarySearchTree (rInsert and rContains)

--- src/main/java/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/main/java/datastructures/binarysearchtree/BinarySearchTree.java
@@ -53,4 +53,38 @@ public class BinarySearchTree {
 
         return false;
     }
+
+    // recursive methods
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+    public Node rInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+
+        return currentNode;
+    }
 }
